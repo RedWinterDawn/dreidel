37b7cff095ee140b2a2a7f29379d3e71ec1f8c7f
from-nibly <jdavidson@getjive.com>
2014-05-21 16:43:40 -0600

    Added stuff for keith to start working on

---
+++ dreidel-goyim/src/test/java/com/jive/qa/dreidel/goyim/service/ExampleServiceIT.java
@@ -0,0 +1,39 @@
+package com.jive.qa.dreidel.goyim.service;
+
+import org.junit.Test;
+
+import com.jive.myco.jazz.config.inject.JazzServiceConfigurationModule;
+import com.jive.myco.jazz.test.launcher.JazzServiceTestLauncher;
+
+/**
+ * Example of of a local integration test that uses the
+ * {@link JazzServiceTestLauncher test launcher}.
+ * 
+ * @author David Valeri
+ */
+public class ExampleServiceIT
+{
+  @Test
+  public void test() throws Exception
+  {
+    try (
+        // Implements closeable so you can be sure it gets killed on the way out of your test 
+        // method.
+        final JazzServiceTestLauncher launcher = JazzServiceTestLauncher.builder()
+            // Name it
+            .serviceName("jazz-examples-jazz-service")
+            // Add the modules you want to launch
+            .withModule(ExampleServiceModule.class)
+            .withModule(JazzServiceConfigurationModule.class)
+            // Set some properties provided to your service via the launcher
+            .withProperty("arg1", "Au revoir!")
+            .build())
+    {
+      // Start it up
+      launcher.init();
+      
+      // Wait a little so it spits out some logs
+      Thread.sleep(2000);
+    }
+  }
+}
